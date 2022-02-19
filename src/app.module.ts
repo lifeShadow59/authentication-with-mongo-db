986560a42f404a997e805b792413760538fdcc5e
Abhishek Dhanani <abhishek9712a@gmail.com>
2022-02-20 03:17:27 +0530

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    MongooseModule.forRoot(process.env.DATABASE_URL)
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
